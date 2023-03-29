3b8cf5b282fc41de91fd7e6777588d5668f21a96
beginner1986 <beginner1986@gmail.com>
2023-03-29 22:04:09 +0200

    Move right implemented

--- 2048Game/beginner1986/js/script.js
+++ 2048Game/beginner1986/js/script.js
@@ -14,16 +14,51 @@ const fillTwoRandomFields = (_board) => {
     }
 }
 
+const clearBoard = () => {
+    const parent = document.querySelector('#board');
+
+    while(parent.firstChild)
+        parent.removeChild(parent.firstChild);
+};
+
 const renderBoard = () => {
     const parent = document.querySelector('#board');
+
+    clearBoard();
 
     for(i=0; i<board.length; i++) {
         const div = document.createElement('div');
         div.classList.add('board-item');
-        div.textContent = board[i];
+
+        if(board[i] != 0)
+            div.textContent = board[i];
+        
         parent.append(div);
     }
+};
+
+const moveRight = (_board) => {
+    for(i=0; i<16; i++) {
+        if(i % 4 != 3 && board[i] !== 0 && board[i+1] === 0) {
+            _board[i + 1] = _board[i];
+            _board[i] = 0;
+        }
+    }
+
+    renderBoard();
+    
+    console.log("right");
+    console.log(board);
 }
+
+document.addEventListener("keydown", (event) =>
+{
+    switch(event.key) {
+        case 'ArrowRight':
+            moveRight(board);
+            break;
+    }
+});
 
 fillTwoRandomFields(board);
 renderBoard();
